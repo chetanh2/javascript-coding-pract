1645e9ba7132ab3211b719203b2e33fd9b69ff5a
chetan <chetan@osiansoftware.com>
2025-03-30 13:01:54 +0530

    added new changes

--- topics/DSA-Arrays/arrayDeclaration.js
+++ topics/DSA-Arrays/arrayDeclaration.js
@@ -13,17 +13,19 @@
 let arr = ["apple","banana","cherry"]
 
 // ? Add to end of the array
-arr.push("orange")
+// arr.push("orange")
 
 // ? remove from end of the array
 // arr.pop()
 // arr.pop() 
 
 // ? Add to the Top of the Array
-arr.unshift("orange")
+// arr.unshift("orange")
+// console.log(arr);
+
 
 // ? Remove from Top of the Array
-arr.shift()
+// arr.shift()
 // console.log(arr);
 
 //! Looping in array
@@ -31,11 +33,10 @@ arr.shift()
 for (let i = 0; i < arr.length; i++) {
     const element = arr[i];
     // console.log(element);
-    
 }
 
 let i = 0
-while(i<arr.length){
+while(i < arr.length){
     // console.log("hello",arr[i]);
 
     i++
@@ -43,5 +44,46 @@ while(i<arr.length){
 
 // ! Inbuilt Loop methods
 const numbers = [1,2,3,4,5]
+// ? map in js
+// const newNum = numbers.map((item,index,array)=>{
+//     return item + 5
+// } )
+// const newNum = numbers.map((item,index,array)=> item + 5)
+// console.log(newNum);
 
-//? map in js
+//? filter in js // filter takes condition
+// const newNum = numbers.filter((item,index,array)=>{
+//     return item > 3
+// } )
+// console.log(newNum);
+
+// ? Reduce in js // reduce takes 4 paramters => accumulator, currentValue, currentIndex, array
+// ? reduce takes an arrya & reduces its value down to one value
+const sum = numbers.reduce((prev,item)=>{
+    return prev + item
+} ,2)
+// console.log(sum);
+
+//? some in js
+const res = numbers.some((item)=> item > 3)
+// console.log(res);
+
+// ? every in js
+const res1 = numbers.every((item)=> item > 3)
+// console.log(res1);
+
+//? find in js 
+const res2 = numbers.find((item)=> item > 3)
+// console.log(res2);
+
+// ! Spread & rest Operators
+// ? spread
+const arr1 = [1,2,3]
+const arr2 = [4,5,6]
+const arr3 = [...arr1,...arr2]  //? spread
+console.log(arr3);
+
+// ? rest
+const arr4 = [1,2,3,4,5,6]
+// const [a,b,...arr5] = arr4 //? rest
+// console.log(arr5);
